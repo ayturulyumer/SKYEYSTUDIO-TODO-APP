c55c6b1819913d41593196f0be4732b2afee86dd
ajummer <makeudreamempty@gmail.com>
2025-03-01 16:04:29 +0200

    feat: add task list

---
+++ client/components/TaskList.jsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useTaskContext } from "../context/TaskContext.jsx"
+import TaskItem from "./TaskItem.jsx"
+import EmptyTaskList from "./EmptyTaskList.jsx"
+
+export default function TaskList() {
+    const { filteredTasks } = useTaskContext()
+
+    if (filteredTasks.length === 0) {
+        return (
+            <EmptyTaskList />
+        )
+    }
+
+    
+    return (
+        <ul className="space-y-2">
+            {filteredTasks.map((task) => (
+                <li key={task.id}>
+                    <TaskItem task={task} />
+                </li>
+            ))}
+        </ul>
+    )
+}
+
