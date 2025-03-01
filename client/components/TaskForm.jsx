5fe10ed9133199c65071d01c4a92a2a6c842ed20
ajummer <makeudreamempty@gmail.com>
2025-03-01 16:52:51 +0200

    feat: add required to task description

--- client/components/TaskForm.jsx
+++ client/components/TaskForm.jsx
@@ -88,6 +88,7 @@ export default function TaskForm() {
                                         placeholder="Enter task description"
                                         className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white"
                                         rows="3"
+                                        required
                                     ></textarea>
                                 </div>
                             </div>
